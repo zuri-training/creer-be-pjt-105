bb44902c1005f25d04891eb5de5d9925c55ad896
Bell Oluwatomisin <bello4aus@gmail.com>
2021-06-20 23:16:57 +0100

    delete commit

--- config/settings.py
+++ config/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from environs import Env
+import environ
 import django_heroku
 
-env = Env()
-env.read_env()
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = env('DEBUG')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -91,7 +92,8 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': env.dj_db_url("DATABASE_URL")
+    'default': env.db(),
+    'extra': env.db('SQLITE_URL', default='sqlite:///db.sqlite3')
 }
 
 
