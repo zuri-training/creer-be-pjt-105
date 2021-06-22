a16c94d4b24af4fa758c5872f0ccf4b834761665
Bell Oluwatomisin <bello4aus@gmail.com>
2021-06-22 11:10:12 +0100

    added swagger

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Authentication Web Api",
+      default_version='v1',
+      description="This is the API for Login, Register",
+      terms_of_service="https://mycreer/terms/",
+      contact=openapi.Contact(email="contact@contacts.remote"),
+      license=openapi.License(name="Test License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('authentication.urls'))
+    path('api/auth/', include('authentication.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
+    path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
