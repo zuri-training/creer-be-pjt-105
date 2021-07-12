49ccf32023d8ecc062a40da5a4f2c75aca41d3e8
Bell Oluwatomisin <bello4aus@gmail.com>
2021-07-12 13:18:28 +0100

    rm api des

--- config/urls.py
+++ config/urls.py
@@ -27,7 +27,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="Authentication Web Api",
         default_version='v1',
-        description="Welcome to the api for Creer, Base Url: creer-pjt-105.herokuapp.com/api , Our api makes use of simplejwt token with a Bearer authorization. Register: You get an access token and refesh token which you save in your local storage.",
+        description="This is the API for Creer",
         terms_of_service="https://mycreer/terms/",
         contact=openapi.Contact(email="bello4aus@gmail.com"),
         license=openapi.License(name="Test License"),
